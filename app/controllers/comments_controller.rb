774f53578f4355a09ff5138231ee7cc195503f3d
shndrs <sahandraeisi1994@gmail.com>
2020-01-06 23:05:31 +0330

    Added Posts, Users and Comments controller

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,13 @@
+class CommentsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,27 @@
 Rails.application.routes.draw do
+  get 'comments/index'
+
+  get 'comments/new'
+
+  get 'comments/show'
+
+  get 'comments/destroy'
+
+  get 'users/index'
+
+  get 'users/new'
+
+  get 'users/show'
+
+  get 'users/destroy'
+
+  get 'posts/index'
+
+  get 'posts/new'
+
+  get 'posts/show'
+
+  get 'posts/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
