c7e2a997fe18b407ca0613314c0d41d8ba6b7d45
shndrs <sahandraeisi1994@gmail.com>
2020-01-10 23:26:55 +0330

    Added Doc model

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200106191845) do
+ActiveRecord::Schema.define(version: 20200110195447) do
 
   create_table "comments", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "docs", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
