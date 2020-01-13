a4a77bf4b69e9f6608ac6ea7fb786d071c562723
shndrs <sahandraeisi1994@gmail.com>
2020-01-13 22:18:24 +0330

    Added show view and Updated docs_controller

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,9 +1,12 @@
 class DocsController < ApplicationController
 
+  before_action :find_doc, only: [:show, :edit, :update, :destroy]
+
   def index
   end
 
   def show
+
   end
 
   def new
@@ -31,7 +34,8 @@ class DocsController < ApplicationController
 
   private
 
-  def find_docs
+  def find_doc
+    @doc = Doc.find(params[:id])
   end
 
   def doc_params
