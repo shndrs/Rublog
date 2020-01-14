d7310e1f21a5fb6e1b9fabfe28966074c2d82481
shndrs <sahandraeisi1994@gmail.com>
2020-01-14 12:22:09 +0330

    Association between User and Files

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200113193049) do
+ActiveRecord::Schema.define(version: 20200114084043) do
 
   create_table "comments", force: :cascade do |t|
     t.string "title"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20200113193049) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "posts", force: :cascade do |t|
